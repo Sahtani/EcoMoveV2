fa3726e4a01051bee07898f6faa614c8ba2575d9
Sahtani <soumia.sahtani@gmail.com>
2024-09-06 15:21:45 +0100

    add Ticket entity

---
+++ src/Models/Entities/Ticket.java
@@ -0,0 +1,100 @@
+package Models.Entities;
+
+import Config.Db;
+import Models.Enums.TicketStatus;
+
+import java.sql.*;
+import java.util.UUID;
+
+import Models.Enums.TransportType;
+
+
+public class Ticket {
+
+
+    private UUID id;
+    private TransportType transportType;
+    private float purchasePrice;
+    private float salePrice;
+    private Timestamp saleDate;
+    private TicketStatus ticketStatus;
+    private UUID contractId;
+    private Connection connection;
+
+
+    // Constructor
+    public Ticket(UUID id, TransportType transportType, float purchasePrice, float salePrice, Timestamp saleDate, TicketStatus ticketStatus) {
+        this.id = id;
+        this.transportType = transportType;
+        this.purchasePrice = purchasePrice;
+        this.salePrice = salePrice;
+        this.saleDate = saleDate;
+        this.ticketStatus = ticketStatus;
+    }
+
+    // constructor
+    public Ticket() {
+        this.connection = Db.getInstance().getConnection();
+    }
+
+
+    //Getters
+
+    public UUID getId() {
+        return id;
+    }
+
+    public TransportType getTransportType() {
+        return transportType;
+    }
+
+    public float getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public float getSalePrice() {
+        return salePrice;
+    }
+
+    public Timestamp getSaleDate() {
+        return saleDate;
+    }
+
+    public TicketStatus getTicketStatus() {
+        return ticketStatus;
+    }
+
+    public UUID getContractId() {
+        return contractId;
+    }
+    //Setters
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public void setTransportType(TransportType transportType) {
+        this.transportType = transportType;
+    }
+
+    public void setPurchasePrice(float purchasePrice) {
+        this.purchasePrice = purchasePrice;
+    }
+
+    public void setSalePrice(float salePrice) {
+        this.salePrice = salePrice;
+    }
+
+    public void setSaleDate(Timestamp saleDate) {
+        this.saleDate = saleDate;
+    }
+
+    public void setTicketStatus(TicketStatus ticketStatus) {
+        this.ticketStatus = ticketStatus;
+    }
+
+    public void setContractId(UUID contractId) {
+        this.contractId = contractId;
+    }
+
+}
