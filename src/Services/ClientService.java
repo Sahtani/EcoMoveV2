10f3b9065cdda7ffda961489d821ec58987f15e4
Sahtani <soumia.sahtani@gmail.com>
2024-09-08 19:53:08 +0100

    add ClientServie

--- src/Services/ClientService.java
+++ src/Services/ClientService.java
@@ -1,4 +1,22 @@
 package Services;
 
+import Dao.ClientDAO;
+import Models.Entities.Client;
+
+import java.util.UUID;
+
 public class ClientService {
+
+    private ClientDAO clientDAO;
+
+    public ClientService(){
+        this.clientDAO = new ClientDAO();
+    }
+
+    // add Client :
+
+    public String createClient(UUID id, String firstName, String lastName, String email , int phoneNumber){
+        Client client = new Client(id , firstName, lastName, email, phoneNumber) ;
+        return String.valueOf(clientDAO.Create(client));
+    }
 }
