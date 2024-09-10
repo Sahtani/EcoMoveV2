baab72f403fb6bea4007f64d1a688723fe6901e4
Sahtani <soumia.sahtani@gmail.com>
2024-09-10 14:57:11 +0100

    Fix getAllContracts method to properly fetch and map results

--- src/Dao/Implementations/ContractDAO.java
+++ src/Dao/Implementations/ContractDAO.java
@@ -1,6 +1,7 @@
 package Dao.Implementations;
 
 import Config.Db;
+import Dao.Interfaces.ContractDaoInterface;
 import Models.Entities.Contract;
 import Models.Enums.ContractStatus;
 
@@ -8,125 +9,131 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
-public class ContractDAO {
+public class ContractDAO implements ContractDaoInterface {
 
     private Connection connection;
     public ContractDAO(){
         this.connection= Db.getInstance().getConnection();
     }
 
-    // display list of contracts
 
-    public ResultSet getAllContracts() {
-        String sql = "SELECT * FROM contracts";
-        ResultSet resultPartners = null;
 
-        try {
+    @Override
+    public boolean addContract(Contract contract) {
+        String sql = "INSERT INTO contracts (id, partner_id, start_date, end_date, special_rate, agreement_conditions, renewable, contract_status) " +
+                "VALUES (?, ?, ?, ?, ?, ?, ?, CAST(? AS statusContract))";
 
-            PreparedStatement stmt = connection.prepareStatement(sql);
-
-            resultPartners = stmt.executeQuery();
-
-        } catch (Exception exception) {
-            System.out.println("Statement Exception: " + exception.getMessage());
-            exception.printStackTrace();
-        }
-
-        return resultPartners;
-    }
-
-     //Insert a new contract :
-
-    public String create(Contract contract) {
-        String sql = "INSERT INTO contracts (id, partner_id, start_date, end_date, special_rate, agreement_conditions, renewable, contract_status) VALUES (?, ?, ?, ?, ?, ?, ?,CAST(? AS statusContract))";
-
-        String resultMessage = null;
-        try {
-            PreparedStatement pstmt = connection.prepareStatement(sql);
+        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
             pstmt.setObject(1, contract.getId());
             pstmt.setObject(2, contract.getPartnerId());
-            pstmt.setObject(3, contract.getStartDate());
+            pstmt.setObject(3, java.sql.Date.valueOf(contract.getStartDate()));
+
             if (contract.getEndDate() != null) {
-                pstmt.setObject(4, contract.getEndDate());
+                pstmt.setObject(4, java.sql.Date.valueOf(contract.getEndDate()));
             } else {
                 pstmt.setNull(4, java.sql.Types.DATE);
             }
+
             pstmt.setFloat(5, contract.getSpecialRate());
             pstmt.setString(6, contract.getAgreementConditions());
             pstmt.setBoolean(7, contract.isRenewable());
 
-            ContractStatus status = contract.getContractStatus();
-            if (status != null) {
-
-                pstmt.setString(8, status.name());
+            if (contract.getContractStatus() != null) {
+                pstmt.setString(8, contract.getContractStatus().name());
             } else {
                 pstmt.setNull(8, java.sql.Types.VARCHAR);
             }
 
-
             pstmt.executeUpdate();
-            resultMessage = "Contract stored successfully.";
+            return true;
 
         } catch (SQLException e) {
             e.printStackTrace();
+            return false;
         }
-        return resultMessage;
     }
 
-    //update contract
-    public boolean update(Contract contract) {
 
-        String query = "UPDATE contracts SET start_date = ?, end_date = ?, special_rate = ?, agreement_conditions = ?, renewable = ?, contract_status = ? WHERE id = ?";
+    @Override
+    public boolean updateContract(Contract contract) {
 
-        try {
-            PreparedStatement pstmt = connection.prepareStatement(query);
+            String query = "UPDATE contracts SET start_date = ?, end_date = ?, special_rate = ?, agreement_conditions = ?, renewable = ?, contract_status = ? WHERE id = ?";
+
+            try {
+                PreparedStatement pstmt = connection.prepareStatement(query);
 
 //            pstmt.setObject(1, getPartnerId(), java.sql.Types.OTHER);
-            pstmt.setObject(1,contract.getStartDate(), java.sql.Types.DATE);
-            pstmt.setDate(2, java.sql.Date.valueOf(contract.getEndDate()));
-            pstmt.setFloat(3,contract.getSpecialRate());
-            pstmt.setString(4,contract.getAgreementConditions());
-            pstmt.setBoolean(5,contract.isRenewable());
-            pstmt.setObject(6,contract.getContractStatus().toString(), java.sql.Types.OTHER);
-            pstmt.setObject(7,contract.getId(), java.sql.Types.OTHER);
+                pstmt.setObject(1,contract.getStartDate(), java.sql.Types.DATE);
+                pstmt.setDate(2, java.sql.Date.valueOf(contract.getEndDate()));
+                pstmt.setFloat(3,contract.getSpecialRate());
+                pstmt.setString(4,contract.getAgreementConditions());
+                pstmt.setBoolean(5,contract.isRenewable());
+                pstmt.setObject(6,contract.getContractStatus().toString(), java.sql.Types.OTHER);
+                pstmt.setObject(7,contract.getId(), java.sql.Types.OTHER);
 
-            int rowsUpdated = pstmt.executeUpdate();
-            if (rowsUpdated > 0) {
-                System.out.println("Contract updated successfully.");
-            } else {
-                System.out.println("No contract found with the provided id.");
+                int rowsUpdated = pstmt.executeUpdate();
+                return rowsUpdated > 0;
+            } catch (SQLException e) {
+                System.err.println("Error updating contract: " + e.getMessage());
             }
-        } catch (SQLException e) {
-            System.err.println("Error updating contract: " + e.getMessage());
+
+            return false;
         }
 
-        return false;
-    }
 
-    //Remove sepcific Contract :
 
-    public String delete(UUID id) {
-        String resultMessage;
+
+    @Override
+    public boolean deleteContract(UUID id) {
         try {
             PreparedStatement pstmt = connection.prepareStatement("DELETE FROM contracts WHERE id = ?");
             pstmt.setObject(1, id, java.sql.Types.OTHER);
             int rowsAffected = pstmt.executeUpdate();
 
-            if (rowsAffected > 0) {
-                resultMessage = "Partner deleted successfully.";
-            } else {
-                resultMessage = "No partner found with the provided UUID.";
-            }
+            return rowsAffected > 0;
         } catch (Exception exception) {
             exception.printStackTrace();
-            resultMessage = "An error occurred while trying to delete the partner.";
+
         }
-        return resultMessage;
+
+        return false;
     }
 
+    // display list of contracts
+    @Override
+    public List<Contract> getAllContracts() {
+        String sql = "SELECT * FROM contracts";
+        List<Contract> contracts = new ArrayList<>();
 
+        try (PreparedStatement stmt = connection.prepareStatement(sql);
+             ResultSet resultSet = stmt.executeQuery()) {
+
+            while (resultSet.next()) {
+                UUID id = (UUID) resultSet.getObject("id");
+                UUID partnerId = (UUID) resultSet.getObject("partner_id");
+                LocalDate startDate = resultSet.getDate("start_date").toLocalDate();
+                LocalDate endDate = resultSet.getDate("end_date") != null ? resultSet.getDate("end_date").toLocalDate() : null;
+                float specialRate = resultSet.getFloat("special_rate");
+                String agreementConditions = resultSet.getString("agreement_conditions");
+                boolean renewable = resultSet.getBoolean("renewable");
+                ContractStatus contractStatus = ContractStatus.valueOf(resultSet.getString("contract_status"));
+
+                Contract contract = new Contract(id, partnerId, startDate, endDate, specialRate, agreementConditions, renewable, contractStatus);
+                contracts.add(contract);
+            }
+
+        } catch (SQLException e) {
+            System.out.println("SQL Exception: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+        return contracts;
+    }
 
 
 
--- src/Dao/Interfaces/ContractDaoInterface.java
+++ src/Dao/Interfaces/ContractDaoInterface.java
@@ -1,4 +1,14 @@
 package Dao.Interfaces;
 
+import Models.Entities.Contract;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface ContractDaoInterface {
+
+    boolean addContract(Contract contract);
+    boolean updateContract(Contract contract);
+    List<Contract> getAllContracts();
+    boolean deleteContract(UUID id);
 }
