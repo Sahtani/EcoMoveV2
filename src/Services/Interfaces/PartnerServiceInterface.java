8af56defca822033157ca6bcb53e46a5c1bb0a5e
Sahtani <soumia.sahtani@gmail.com>
2024-09-11 12:13:18 +0100

    Add PartnerServiceInterface with CRUD operations

---
+++ src/Services/Interfaces/PartnerServiceInterface.java
@@ -0,0 +1,14 @@
+package Services.Interfaces;
+
+import Models.Entities.Partner;
+
+import java.sql.ResultSet;
+import java.util.UUID;
+
+public interface PartnerServiceInterface {
+
+    boolean addPartner(Partner partner);
+    ResultSet getAllPartners();
+    boolean updatePartner(Partner partner);
+    boolean  deletePartner(UUID id);
+}
