619a8e9bbb5e6dc22f16167f42ce46986c11f4a7
Sahtani <soumia.sahtani@gmail.com>
2024-09-11 11:02:15 +0100

    Add getters and setters for Reservation class attributes

--- src/Models/Entities/Reservation.java
+++ src/Models/Entities/Reservation.java
@@ -1,4 +1,50 @@
 package Models.Entities;
 
+import java.time.LocalDate;
+
 public class Reservation {
+
+    private int id;
+    private LocalDate reservationDate ;
+    private float totalPrice ;
+    private boolean status ;
+    private Client client ;
+
+
+    // getters and setters
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDate getReservationDate() {
+        return reservationDate;
+    }
+    public void setReservationDate(LocalDate reservationDate) {
+        this.reservationDate = reservationDate;
+    }
+
+    public float getTotalPrice() {
+        return totalPrice;
+    }
+    public void setTotalPrice(float totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
 }
